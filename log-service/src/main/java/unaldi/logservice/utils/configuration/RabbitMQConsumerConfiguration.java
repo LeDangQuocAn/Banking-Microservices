06df62999ee88e71de5bc4942b676485d09dc9a5
LeDangQuocAn <23520008@gm.uit.edu.vn>
2026-03-24 20:52:56 +0700

    fix CI pipeline error

--- log-service/src/main/java/unaldi/logservice/utils/configuration/RabbitMQConsumerConfiguration.java
+++ log-service/src/main/java/unaldi/logservice/utils/configuration/RabbitMQConsumerConfiguration.java
@@ -1,9 +1,11 @@
 package unaldi.logservice.utils.configuration;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
@@ -17,9 +19,17 @@ import org.springframework.messaging.handler.annotation.support.DefaultMessageHa
  */
 @Configuration
 public class RabbitMQConsumerConfiguration implements RabbitListenerConfigurer {
+    @Value("${rabbitmq.logs.queue}")
+    private String logsQueue;
+
     @Bean
     public MappingJackson2MessageConverter jackson2Converter() {
         return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    public Queue logsQueue() {
+        return new Queue(logsQueue, true);
     }
 
     @Bean
