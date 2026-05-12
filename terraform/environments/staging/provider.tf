388e86156e4ec65d404ca0d75fbf3a22c04103ad
LeDangQuocAn <23520008@gm.uit.edu.vn>
2026-05-12 11:00:03 +0700

    feat: add ebs-sc storage class for csi driver

--- terraform/environments/staging/provider.tf
+++ terraform/environments/staging/provider.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.70.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.8.0"
@@ -29,4 +33,14 @@ provider "aws" {
       Owner       = "KhoiP"
     }
   }
+}
+
+data "aws_eks_cluster_auth" "staging" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_data)
+  token                  = data.aws_eks_cluster_auth.staging.token
 }
